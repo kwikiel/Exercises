10bc029e8aad786bd4d21f5719e03c259c9e19b6
Kacper Wikiel <k.wikiel@gmail.com>
2021-05-11 17:04:30 +0200

    Add solution to the multiples exercise

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        HashSet<int> m = new HashSet<int>();
+
+        foreach (int num in multiples)
+        {
+            // Case if num is zero 
+            if (num == 0)
+            {
+                continue;
+            }
+            int i = 1;
+            while (num * i < max)
+            {
+                if (num * i < max)
+                {
+                    m.Add(num * i);
+                }
+                i++;
+            }
+        }
+        var total = m.Sum();
+        return total;
     }
 }
