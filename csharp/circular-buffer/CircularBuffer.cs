74ba0762d6787d7ad642e272e22e3310054a7b89
Kacper Wikieł <kacper.wikiel@igoriatrade.com>
2021-05-11 14:35:10 +0200

    Add various solutions to exercism repo

---
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class CircularBuffer<T>
+{
+    private T[] array;
+    int head;
+    int size; 
+    public CircularBuffer(int capacity)
+    {
+        array = new T[capacity];
+        size = 0; 
+    }
+
+    public T Read()
+    {
+        if(size == 0)
+        {
+            throw new InvalidOperationException("You need to implement this function.");
+        }
+        return array[0];
+    }
+
+    public void Write(T value)
+    {
+        size++;
+    }
+
+    public void Overwrite(T value)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] inputPieces = input.ToCharArray();
+        Array.Reverse(inputPieces);
+
+        string output = new string(inputPieces);
+        return output;
+
+    }
+}
